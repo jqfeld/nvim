0fb9b6ab753a24c070e648452573cbd8922c4776
Jan Kuhfeld <jan.kuhfeld@rub.de>
2021-03-08 16:18:26 +0100

    fixed julials problem in on_new_config under win10

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -78,8 +78,31 @@ nvim_lsp.rust_analyzer.setup{
   }
 }
 
+
+local julia_cmd =  {
+  "julia",
+  "--startup-file=no",
+  "--history-file=no",
+  "-e", [[
+    using Pkg;
+    Pkg.instantiate()
+    using LanguageServer; using SymbolServer;
+    depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
+    project_path = dirname(something(Base.current_project(pwd()), Base.load_path_expand(LOAD_PATH[2])))
+    # Make sure that we only load packages from this environment specifically.
+    @info "Running language server" env=Base.load_path()[1] pwd() project_path depot_path
+    server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path);
+    server.runlinter = true;
+    run(server);
+  ]]
+};
+
 nvim_lsp.julials.setup{
-  on_attach = on_attach
+    cmd = julia_cmd,
+    on_new_config = function(new_config,_) 
+        new_config.cmd = julia_cmd 
+    end,
+    on_attach = on_attach
 }
 
 nvim_lsp.texlab.setup{
